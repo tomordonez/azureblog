5d4f2f110efd8ec70fe564c99144183beaf7a827
Tom Ordonez <tomordonezworks@gmail.com>
2023-02-21 23:50:33 -0600

    Complete creating app

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,11 +22,22 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          I am a data analyst using Python, SQL, and Power BI. Contact me on <a href="https://www.linkedin.com/in/tomordonez/">Linkedin</a>.
+          I am a data analyst using Python, SQL, and Power BI. Contact me on <a href="https://www.linkedin.com/in/tomordonez/">Linkedin</a>. This is a blog about Azure and Data Analytics.
         </p>
-        <p>
-          This is a blog about Azure and Data Analytics.
-        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ posts/pre-rendering.md
@@ -0,0 +1,6 @@
+---
+title: 'Two Forms of Pre-rendering'
+date: '2020-01-01'
+---
+
+Next.js has two forms of pre-rendering
